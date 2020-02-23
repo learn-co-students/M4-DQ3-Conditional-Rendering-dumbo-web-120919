5540017f737fb6cb4c5d8e5a77b1ed4ba793e12e
Annie Liao <liaowow@gmail.com>
2020-02-23 11:48:04 -0500

    Done.

--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import MenuBar from '../components/MenuBar.js'
-import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
+import { Profile, Photos, Cocktails, Pokemon } from '../components/Pages.js'
 
 class MainBox extends React.Component {
 
+  state = {
+    selected: "profile"
+  }
+
+  changeSelected = (selected) => {
+    this.setState({
+      selected: selected
+    })
+  }
 
   render() {
 
@@ -13,12 +22,31 @@ class MainBox extends React.Component {
 
     */
 
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
+    let details;
+
+    switch(this.state.selected) {
+      case "profile":
+        details = <Profile />
+        break
+      case "photo":
+        details = <Photos />
+        break
+      case "cocktail":
+        details = <Cocktails />
+        break
+      case "pokemon":
+        details = <Pokemon />
+        break
+      default:
+        details = null
+        break
+    }
+
 
     return (
       <div>
-        <MenuBar />
-        {detailsToDisplay}
+        <MenuBar changeSelected={this.changeSelected} selected={this.state.selected}/>
+        {details}
       </div>
     )
   }
